73369a0389f01a86e4825a2c0f5c0d3d9a8e8898
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2024-01-24 10:11:33 +0100

    Ajout des contraintes avec PropTypes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Card({ title, ...props }) {
+function Card({ title = "Titre", ...props }) {
   return (
-    <article className="card">
+    <article className="card" {...props}>
       <header className="card__header header__title">{title}</header>
       <section className="card__main">Content</section>
     </article>
   );
 }
+
+Card.propTypes = {
+  title: PropTypes.string,
+};
 
 export default Card;
